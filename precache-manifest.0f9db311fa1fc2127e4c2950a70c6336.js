9913c68a9b0a15a7685802a7a7d989584282a045
Nathan Smith <nathan.smith@bentley.com>
2019-10-18 09:49:26 -0500

    Updates

--- precache-manifest.0f9db311fa1fc2127e4c2950a70c6336.js
+++ precache-manifest.0f9db311fa1fc2127e4c2950a70c6336.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d016071d43f550e1fc680b8f0755e4d",
+    "revision": "1129397dd4165eca1e7b33bf8ff16913",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.8c467cd4.chunk.css"
   },
   {
-    "revision": "c49f9a492fa5f6b6aa9c",
+    "revision": "3973d4c8b80732f35e7e",
     "url": "/static/css/main.61c2ff08.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.ebb93707.chunk.js"
   },
   {
-    "revision": "c49f9a492fa5f6b6aa9c",
-    "url": "/static/js/main.65904e3b.chunk.js"
+    "revision": "3973d4c8b80732f35e7e",
+    "url": "/static/js/main.308cf5d6.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69d9b19e743dee9db49929cbeaa37089.js"
+  "/precache-manifest.0f9db311fa1fc2127e4c2950a70c6336.js"
 );
 
 self.addEventListener('message', (event) => {
